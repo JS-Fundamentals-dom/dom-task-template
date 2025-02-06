4f51ae7e330d5a7c599267e3d79fb5246acf33d4
Vladyslav Khrychov <vladkhrychov@gmail.com>
2025-02-06 17:43:49 +0200

    Add task 3 and move runscript call  inside before each

--- tests/task1.test.js
+++ tests/task1.test.js
@@ -14,6 +14,7 @@ describe("Тестування роботи, підключення в body", ()
       "utf-8"
     );
     logSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+    runScript();
   });
   afterEach(() => {
     logSpy.mockRestore();
@@ -39,7 +40,6 @@ describe("Тестування роботи, підключення в body", ()
             </ul>
         `;
 
-    runScript();
     expect(logSpy).toHaveBeenNthCalledWith(1, "1");
     expect(logSpy).toHaveBeenNthCalledWith(2, "5");
     expect(logSpy).toHaveBeenNthCalledWith(3, "2");
@@ -62,7 +62,6 @@ describe("Тестування роботи, підключення в body", ()
             </ul>
         `;
 
-    runScript();
     expect(logSpy).toHaveBeenNthCalledWith(1, "1");
     expect(logSpy).toHaveBeenNthCalledWith(2, "50");
     expect(logSpy).toHaveBeenNthCalledWith(3, "2");
@@ -78,7 +77,6 @@ describe("Тестування роботи, підключення в body", ()
             </ul>
         `;
 
-    runScript();
     expect(logSpy).toHaveBeenNthCalledWith(1, "A");
     expect(logSpy).toHaveBeenNthCalledWith(2, "B");
     expect(logSpy).toHaveBeenNthCalledWith(3, "B");
@@ -97,7 +95,6 @@ describe("Тестування роботи, підключення в body", ()
             </ul>
         `;
 
-    runScript();
     expect(logSpy).toHaveBeenNthCalledWith(1, "A1");
     expect(logSpy).toHaveBeenNthCalledWith(2, "C2");
     expect(logSpy).toHaveBeenNthCalledWith(3, "B1");
@@ -120,8 +117,6 @@ describe("Тестування роботи, підключення в body", ()
                 <li>item-item</li>
             </ul>
         `;
-
-    runScript();
     expect(logSpy).toHaveBeenNthCalledWith(1, "item-01");
     expect(logSpy).toHaveBeenNthCalledWith(2, "item-06");
     expect(logSpy).toHaveBeenNthCalledWith(3, "item-02");
