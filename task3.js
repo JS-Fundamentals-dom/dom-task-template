8ec1bdebf96fac39d1eae3ad7013898873e8211c
Vladyslav Khrychov <vladkhrychov@gmail.com>
2025-02-07 15:09:13 +0200

    Add 4th task and tests

--- task3.js
+++ task3.js
@@ -7,4 +7,5 @@
 //  </main>
 //
 // Цю розмітку потрібно вставити у існуючий html елемент із класом "create_elements"
-
+// Спробуйте не використовувати рядок HTML-коду для вставки (`innerHTML`).
+// Використовуйте методи DOM: `createElement()`, `appendChild()`, `setAttribute()`, `classList.add()` тощо.
